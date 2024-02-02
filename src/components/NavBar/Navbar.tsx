ea27ed97a5872704e9ab30d50ab1a256cb7ea3f4
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-02-02 17:30:34 +0800

    Updated Navbar width

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
 const Wrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 50px;
   display: flex;
   align-items: center;
