3642595d5d6a208e94610b0663ce7efb5a8cd004
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-02-02 16:21:01 +0800

    Initial navbar component

---
+++ src/components/NavBar/Navbar.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Item = styled.div`
+  cursor: pointer;
+`;
+
+const routes = [
+  {
+    name: 'Home',
+    route: '/',
+  },
+  {
+    name: 'Experiences',
+    route: '/experiences',
+  },
+  {
+    name: 'Tech Stack',
+    route: '/tech-stack',
+  },
+];
+
+const Navbar = () => {
+  const history = useHistory();
+  return (
+    <Wrapper>
+      <ItemContainer>
+        {routes.map(({ route, name }) => (
+          <Item
+            key={route}
+            onClick={() => history.push(route)}
+          >
+            {name}
+          </Item>
+        ))}
+      </ItemContainer>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
