2c08fbde5abb744681eac02dda4cf49be54980e9
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-02-02 15:40:12 +0800

    Initial setup of router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+
+import { Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages';
 
 function App() {
-
   return (
-    <></>
-  )
+    <Switch>
+      <Route
+        path={'/'}
+        component={Home}
+      />
+    </Switch>
+  );
 }
 
-export default App
+export default App;
