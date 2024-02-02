47fe16d8f96c96627d8f5a659ec8eb9ba437fc96
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-02-02 16:00:18 +0800

    Initial Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,48 @@
+import styled, { keyframes } from 'styled-components';
+
+const typewriter = keyframes`
+  from { width: 0}
+  to { width: 100% }
+`;
+
+const Greetings = styled.h1`
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 68px;
+  overflow: hidden;
+  white-space: nowrap;
+  animation: ${typewriter} 1.5s steps(30, end);
+`;
+
+const Name = styled(Greetings)`
+  font-size: 80px;
+  animation-name: ${typewriter};
+  animation-delay: 1.1s;
+  animation-fill-mode: both;
+`;
+
+const Role = styled(Greetings)`
+  animation-name: ${typewriter};
+  animation-delay: 2.2s;
+  animation-fill-mode: both;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  bottom: 100px;
+  left: 100px;
+`;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Content>
+        <Greetings>Hello World! 👋🏼</Greetings>
+        <Name>I'm Chris John!</Name>
+        <Role>Full Stack Developer 👨🏽‍💻</Role>
+      </Content>
+    </div>
+  );
 };
 
 export default Home;
